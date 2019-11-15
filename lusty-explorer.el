db7a875027e23c5a4a5c35ceab1fccfc4a396611
Stephen Bach <github@sjbach.com>
2019-11-15 10:48:39 -0500

    Tweaks to the *Lusty-Matches* buffer
    - Truncate long lines; if there are bugs hiding in the layout
      algorithm or the calculation of the accessible window dimensions, we
      may print a line of completions that is longer than the displayable
      width. With line wrapping, this looks terrible, but with line
      truncation, you probably won't even notice.
    
    - Guard buffer edits with `atomic-change-group` and
      `with-silent-modifications`.
    
    - Set a few minor look-and-feel things: don't display trailing
      whitespace, buffer boundaries or empty lines (should they occur), or
      the horizontal scrollbar.

--- lusty-explorer.el
+++ lusty-explorer.el
@@ -738,17 +738,53 @@ does not begin with '.'."
       (with-current-buffer lusty-buffer
         (setq buffer-read-only t)
         (let ((buffer-read-only nil))
-          (erase-buffer)
-          (lusty--display-matches)
-          (goto-char (point-min))))
+              (when (and (boundp 'visual-line-mode)
+                         visual-line-mode)
+                ;; Remove visual-line-mode if it's enabled to make wrapping --
+                ;; which we don't want, and which shouldn't happen -- look a
+                ;; little better. This is probably not necessary or useful
+                ;; given we're setting `truncate-lines` below.
+                (visual-line-mode -1))
+              (unless truncate-lines
+                ;; More gracefully handle any remaining bugs in the layout
+                ;; algorithm. If an inserted line of completions happens
+                ;; to be longer than the window's text body -- which
+                ;; shouldn't happen -- don't wrap the line, just show a
+                ;; truncation indicator in the fringe (or, if there's no
+                ;; fringe, in the final text column).
+                ;;
+                ;; The below is most of what `(toggle-truncate-lines 1)` does,
+                ;; but without emitting a noisy line to *Messages*.
+                (setq truncate-lines t)
+                (let ((window (get-buffer-window lusty-buffer)))
+                  (when window
+                    (set-window-hscroll window 0))))
+              ;; Minor look-and-feel tweaks. We disable these display settings
+              ;; in the completions buffer in case the user has enabled them
+              ;; globally. Is this overreaching? Maybe, I'm not sure.
+              (when indicate-buffer-boundaries
+                (setq indicate-buffer-boundaries nil))
+              (when show-trailing-whitespace
+                (setq show-trailing-whitespace nil))
+              ;; (Probably not necessary or useful.)
+              (when indicate-empty-lines
+                (setq indicate-empty-lines nil))
+              ;; There is also `overflow-newline-into-fringe`, which would best
+              ;; be t: "If nil, also continue lines which are exactly as wide
+              ;; as the window"; but it can't be set buffer-local.
+              (with-silent-modifications
+                (atomic-change-group
+                  (erase-buffer)
+                  (lusty--display-matches)))
+              (goto-char (point-min))
+              (set-buffer-modified-p nil)))
 
-      ;; If only our matches window is open,
+      ;; If our matches window has somehow become the only window:
       (when (one-window-p t)
         ;; Restore original window configuration before fitting the
         ;; window so the minibuffer won't grow and look silly.
         (set-window-configuration lusty--initial-window-config))
-      (fit-window-to-buffer (display-buffer lusty-buffer))
-      (set-buffer-modified-p nil))))
+      (fit-window-to-buffer (display-buffer lusty-buffer)))))
 
 (defun lusty-buffer-list ()
   "Return a list of buffers ordered with those currently visible at the end."
